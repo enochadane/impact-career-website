f09ce71df867418fb6e39ac775eaac444ea7a1e9
Banumathi87 <banumathi.thangavelu@tridentsqa.com>
2022-11-23 06:37:30 +0530

    Updated form validation and FAQ

--- frontend/components/Nav/Nav.js
+++ frontend/components/Nav/Nav.js
@@ -1,75 +1,75 @@
-import Link from "next/link";
-import React from "react";
-import Image from "next/image";
-import { useRouter } from "next/router";
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 export default function Nav() {
   const router = useRouter();
 
   return (
     <>
-      <div className='nav'>
-        <section id='home-page'>
-          <nav className='navbar navbar-expand-lg navbar-light bg-white fixed-top p-0 shadow-sm'>
-            <div className='container'>
-              <a href='/' className='navbar-brand'>
+      <div className="nav">
+        <section id="home-page">
+          <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top p-0 shadow-sm">
+            <div className="container">
+              <a href="/" className="navbar-brand">
                 <Image
-                  src='/images/headerLogo.png'
+                  src="/images/headerLogo.png"
                   width={60}
                   height={60}
                 ></Image>
               </a>
               <button
-                className='navbar-toggler'
-                type='button'
-                data-bs-toggle='collapse'
-                data-bs-target='#navbarNavDropdown'
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavDropdown"
               >
-                <span className='navbar-toggler-icon'></span>
+                <span className="navbar-toggler-icon"></span>
               </button>
               <div
-                className='collapse navbar-collapse justify-content-center'
-                id='navbarNavDropdown'
+                className="collapse navbar-collapse justify-content-center"
+                id="navbarNavDropdown"
               >
-                <ul className='navbar-nav ms-auto'>
-                  <li className={(router.pathname == "/about") == "active"}>
-                    <Link className='nav-link resource px-4' href='/about'>
+                <ul className="navbar-nav ms-auto">
+                  <li className={(router.pathname == '/about') == 'active'}>
+                    <Link className="nav-link resource px-4" href="/about">
                       About
                     </Link>
                   </li>
-                  <li className={router.pathname == "/jobs" ? "active" : ""}>
-                    <Link className='nav-link resource px-4' href='/jobs'>
+                  <li className={router.pathname == '/jobs' ? 'active' : ''}>
+                    <Link className="nav-link resource px-4" href="/jobs">
                       Jobs
                     </Link>
                   </li>
 
-                  <li className='nav-item dropdown'>
+                  <li className="nav-item dropdown">
                     <a
-                      className='nav-link'
-                      href='resources'
-                      id='navbarDropdown'
-                      role='button'
-                      data-bs-toggle='dropdown'
-                      aria-expanded='false'
+                      className="nav-link"
+                      href="resources"
+                      id="navbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
                     >
-                      Resources <i className='fa-solid fa-chevron-down'></i>
+                      Resources <i className="fa-solid fa-chevron-down"></i>
                     </a>
                     <ul
-                      className='dropdown-menu shadow border-0'
-                      aria-labelledby='navbarDropdown'
+                      className="dropdown-menu shadow border-0"
+                      aria-labelledby="navbarDropdown"
                     >
                       <li>
                         <a
-                          className='dropdown-item color'
-                          href='/resources#training-Section'
+                          className="dropdown-item color"
+                          href="/resources#training-Section"
                         >
                           Training Resources
                         </a>
                       </li>
                       <li>
                         <a
-                          className='dropdown-item'
-                          href='/resources#interview-Section'
+                          className="dropdown-item"
+                          href="/resources#interview-Section"
                         >
                           Interview Prep
                         </a>
@@ -77,9 +77,9 @@ export default function Nav() {
                     </ul>
                   </li>
                 </ul>
-                <div className='btnDiv'>
-                  <a href='/contact'>
-                    <button className='ContactButton'>contact</button>
+                <div className="btnDiv">
+                  <a href="/contact">
+                    <button className="ContactButton">contact</button>
                   </a>
                 </div>
               </div>
--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Script from 'next/script';
 import Head from 'next/head';
 import Footer from '../components/Footer/Footer';
+import ContactForm from '../components/form/ContactForm';
 
 export default function Home({ posts, name }) {
   return (
@@ -11,9 +12,9 @@ export default function Home({ posts, name }) {
         <title>Contact Us</title>
       </Head>
       <div>
-        <div className='Banner contact-banner'>
-          <div className='container-fluid'>
-            <div className='row m-0'>
+        <div className="Banner contact-banner">
+          <div className="container-fluid">
+            <div className="row m-0">
               <div>
                 <h1>Contact</h1>
               </div>
@@ -23,22 +24,22 @@ export default function Home({ posts, name }) {
       </div>
       {/* cards section */}
       <div>
-        <section id='contact-card-section'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-md-4'>
-                <div className='card border-0'>
-                  <div className='topContent'>
+        <section id="contact-card-section">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <div className="card border-0">
+                  <div className="topContent">
                     <Image
-                      src='/images/find-job.svg'
-                      alt='find-job'
-                      className='img-fluid'
+                      src="/images/find-job.svg"
+                      alt="find-job"
+                      className="img-fluid"
                       width={50}
                       height={50}
                     ></Image>
                     <h2>Find a Job</h2>
                   </div>
-                  <div className='para'>
+                  <div className="para">
                     <p>
                       Find the best contract, temp, or permanent company with
                       the best company through Impact Career
@@ -46,39 +47,39 @@ export default function Home({ posts, name }) {
                   </div>
                 </div>
               </div>
-              <div className='col-md-4'>
-                <div className='card border-0'>
-                  <div className='topContent'>
+              <div className="col-md-4">
+                <div className="card border-0">
+                  <div className="topContent">
                     <Image
-                      src='/images/hire-talent.svg'
+                      src="/images/hire-talent.svg"
                       width={50}
                       height={50}
-                      alt='hire-talent'
-                      className='img-fluid'
+                      alt="hire-talent"
+                      className="img-fluid"
                     ></Image>
                     <h2>Hire a Talent</h2>
                   </div>
-                  <div className='para'>
+                  <div className="para">
                     <p>
                       Let Impact Careers find the best talent for your company
                     </p>
                   </div>
                 </div>
               </div>
-              <div className='col-md-4'>
-                <div className='card border-0'>
-                  <div className='topContent'>
+              <div className="col-md-4">
+                <div className="card border-0">
+                  <div className="topContent">
                     <Image
-                      src='/images/ask-question.svg'
+                      src="/images/ask-question.svg"
                       width={50}
                       height={50}
-                      alt='ask-question'
-                      className='img-fluid'
+                      alt="ask-question"
+                      className="img-fluid"
                     ></Image>
                     <h2>Ask any Question</h2>
                   </div>
                   <div>
-                    <p className='para'>
+                    <p className="para">
                       Do you have any questions for us? The team is ready to
                       respond to any questions, issues, and problems you face.
                     </p>
@@ -89,216 +90,28 @@ export default function Home({ posts, name }) {
           </div>
         </section>
       </div>
-      <div>
-        {/* form section */}
-        <div>
-          <section id='contact-form'>
-            <div className='container'>
-              <div className='get-in-touch'>
-                <h2>Get in Touch</h2>
-                <p>
-                  We do not believe staffing and recruiting are limited to
-                  finding and hiring people to fill open positions. To locate
-                  the most suitable consultant or client for each individual, we
-                  take the time to develop true connections with both parties.
-                </p>
-              </div>
-              <form
-                id='registerForm'
-                className='needs-validation'
-                method='post'
-              >
-                <div className='row'>
-                  <div className='col-sm-6'>
-                    <input
-                      type='text'
-                      className='form-control'
-                      id='firstName'
-                      placeholder='First Name'
-                    />
-                    <div className='invalid-feedback'>
-                      Valid first name is required.
-                    </div>
-                  </div>
 
-                  <div className='col-sm-6'>
-                    <input
-                      type='text'
-                      className='form-control'
-                      id='lastName'
-                      placeholder='Last Name'
-                    />
-                    <div className='invalid-feedback'>
-                      Valid last name is required.
-                    </div>
-                  </div>
-                </div>
-                <div className='row'>
-                  <div className='col-sm-6'>
-                    <input
-                      type='email'
-                      className='form-control'
-                      id='email'
-                      placeholder='Email'
-                    />
-                    <div className='invalid-feedback'>
-                      Valid email address is required.
-                    </div>
-                  </div>
-                  <div className='col-sm-6'>
-                    <input
-                      type='tel'
-                      className='form-control'
-                      id='Phone'
-                      placeholder='Phone'
-                    />
-                    <div className='invalid-feedback'>
-                      Valid phone number is required.
-                    </div>
-                  </div>
-                </div>
-                <div className='row'>
-                  <div className='col-sm-6'>
-                    <input
-                      type='text'
-                      className='form-control'
-                      id='Title'
-                      placeholder='Title'
-                    />
-                    <div className='invalid-feedback'>
-                      Please enter a title .
-                    </div>
-                  </div>
-                  <div className='col-sm-6'>
-                    <input
-                      type='text'
-                      className='form-control'
-                      id='Organization'
-                      placeholder='Organization'
-                    />
-                    <div className='invalid-feedback'>
-                      Please enter the Organization name.
-                    </div>
-                  </div>
-                </div>
-                <div className='row'>
-                  <div className='col-sm-6'>
-                    <input
-                      type='text'
-                      className='form-control'
-                      id='Website'
-                      placeholder=' Website'
-                    />
-                    <div className='invalid-feedback'>
-                      Please enter a website .
-                    </div>
-                  </div>
-                  <div className='col-sm-6'>
-                    <select
-                      className='form-select form-control'
-                      id='Position Type'
-                    >
-                      <option value='Temporary / Contract'>
-                        Temporary / Contract
-                      </option>
-                      <option value='Direct Hire / Permanent'>
-                        Direct Hire / Permanent
-                      </option>
-                      <option value='Executive / Search'>
-                        Executive / Search
-                      </option>
-                      <option value='Payrolling'>Payrolling</option>
-                      <option value='Apply With Us As A Candidate'>
-                        Apply With Us As A Candidate
-                      </option>
-                    </select>
-                    <div className='invalid-feedback'>
-                      Please provide position type.
-                    </div>
-                  </div>
-                </div>
-                <div className='row'>
-                  <div className='col-sm-6'>
-                    <input
-                      type='text'
-                      className='form-control'
-                      id='Position Location'
-                      placeholder='Position Location'
-                    />
-                    <div className='invalid-feedback'>
-                      Please enter position location .
-                    </div>
-                  </div>
-                  <div className='col-sm-6'>
-                    <input
-                      type='text'
-                      className='form-control'
-                      id='Title of Position'
-                      placeholder='Title of Position'
-                    />
-                    <div className='invalid-feedback'>
-                      Please enter Title of Position.
-                    </div>
-                  </div>
-                </div>
-                <div className='row'>
-                  <div className='col-sm-6'>
-                    <div>
-                      <input
-                        type='file'
-                        className='w-file-upload-input form-control UploadImg'
-                        accept='.pdf, .doc, .docx, .txt'
-                        name='Resume-Upload'
-                        data-iconName='fa-solid fa-cloud-arrow-up'
-                        data-name='Upload Job Description, If Available'
-                        aria-hidden='true'
-                        id='Resume-Upload'
-                        placeholder='Upload Job Description, If Available'
-                        // tabindex='-1'
-                      />
-                    </div>
-                  </div>
-                  <div className='col-sm-6'>
-                    <div className='form-group'>
-                      <input
-                        type='text'
-                        className='text-field w-input form-control'
-                        // maxlength='256'
-                        name='How-Did-You-Hear-About-Us'
-                        data-name='How Did You Hear About Us?'
-                        placeholder='How Did You Hear About Us?'
-                        id='How-Did-You-Hear-About-Us'
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className='row'>
-                  <div className='col-md-12'>
-                    <div className='form-group'>
-                      <textarea
-                        placeholder='Or Alternately, Describe Position:'
-                        // maxlength='5000'
-                        id='Message'
-                        name='Message'
-                        data-name='field'
-                        className='form-control'
-                      ></textarea>
-                    </div>
-                  </div>
-                </div>
-                <div className='form-group text-center'>
-                  <input
-                    type='submit'
-                    value='Submit'
-                    data-wait='Please wait...'
-                    className='site-btn'
-                  />
-                </div>
-              </form>
+      {/* form section */}
+      <div>
+        {/* <ContactForm /> */}
+        <section id="contact-form">
+          <div className="container">
+            <div className="get-in-touch">
+              <h2>Get in Touch</h2>
+              <p>
+                We do not believe staffing and recruiting are limited to finding
+                and hiring people to fill open positions. To locate the most
+                suitable consultant or client for each individual, we take the
+                time to develop true connections with both parties.
+              </p>
             </div>
-          </section>
-        </div>
+
+            <ContactForm />
+          </div>
+        </section>
       </div>
+      {/* form section */}
+
       <Footer />
     </div>
   );
