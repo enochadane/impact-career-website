267b06d32e03718f07475e9fc584565126a680d9
Natnael Asmare Mebrate <m.t3sla@gmail.com>
2023-02-07 17:29:30 +0300

    Job search parameters - Content-Type (table)

---
+++ back/src/api/job-search-parameter/controllers/job-search-parameter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * job-search-parameter controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::job-search-parameter.job-search-parameter');
---
+++ back/src/api/job-search-parameter/routes/job-search-parameter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * job-search-parameter router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::job-search-parameter.job-search-parameter');
