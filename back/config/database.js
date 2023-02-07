765f981086a0d5cd9c96e8f7821463a164d0db8d
Natnael Asmare Mebrate <m.t3sla@gmail.com>
2023-02-07 19:58:02 +0300

    SSL access to db

--- back/config/database.js
+++ back/config/database.js
@@ -1,10 +1,15 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "sqlite",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
+      ssl: env("DATABASE_SSL", false),
     },
     useNullAsDefault: true,
   },
