72cf0ef600cd5a33234245ba2a2b8dbe96b0337f
Natnael Asmare Mebrate <m.t3sla@gmail.com>
2023-02-13 11:56:52 +0300

    AWS RDS and S3 integration

--- back/config/database.js
+++ back/config/database.js
@@ -2,15 +2,21 @@ const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "sqlite",
+    client: "postgres",
     connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        env("DATABASE_FILENAME", ".tmp/data.db")
+      host: env(
+        "DATABASE_HOST",
+        "impact-strapi-db.c1rrqhmssrz5.us-east-2.rds.amazonaws.com"
       ),
-      ssl: env("DATABASE_SSL", false),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "76W6HN4P8QC66KFV"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
